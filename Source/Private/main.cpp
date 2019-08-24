d1ebb34fe8d30a3bb9afc1fd8606fe23f971294e
GalacticGlum <nerftactic09@gmail.com>
2019-08-24 09:49:27 -0400

    Staging

--- Source/Private/main.cpp
+++ Source/Private/main.cpp
@@ -4,7 +4,7 @@
  * Project Name: PPiCCG
  * Creation Date: 08/24/19
  * Modified Date: 08/24/19
- * Description: Generates the a large CSV representing the graph
+ * Description: Generates the a CSV representing the graph
  *              of the prime counting (pi) function to its approximation.
  *              This graph helps visualize the limit of the quotient of the
  *              two functions (i.e. how the approximation converges eventually).
