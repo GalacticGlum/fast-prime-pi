70316d706bc59f8cb0d0a88d3acc058f7425831f
GalacticGlum <nerftactic09@gmail.com>
2019-08-25 19:30:45 -0400

    Added legend options to the generate graph script

--- scripts/generate-graph.py
+++ scripts/generate-graph.py
@@ -14,11 +14,17 @@ parser = argparse.ArgumentParser(description='Constructs a graph via matplotlib 
 parser.add_argument('input', type=str, help='The filepath to the CSV file containing the graph data.')
 parser.add_argument('--label', type=str, help='The label of the graph.')
 parser.add_argument('--colour', type=str, help='The colour of the graph.', default='blue')
+parser.add_argument('--xlabel', type=str, help='The label on the x-axis.', default='x')
+parser.add_argument('--ylabel', type=str, help='The label on the y-axis.', default='y')
 parser.add_argument('--xscale', type=str, help='The x-axis scale to apply.', default='linear')
 parser.add_argument('--yscale', type=str, help='The y-axis scale to apply.', default='linear')
 parser.add_argument('--axhline', type=float, help='The horizontal axis line.')
 parser.add_argument('--axhline-style', type=str, help='The style of the horizontal axis line.', default='-')
 parser.add_argument('--axhline-colour', type=str, help='The colour of the horizontal axis line.', default='red')
+parser.add_argument('--legend', dest='legend', help='Enable the legend.', action='store_true')
+parser.add_argument('--no-legend', dest='legend', help='Disable the legend.', action='store_false')
+parser.add_argument('--legend-loc', type=str, help='The location of the legend.', default='upper right')
+parser.set_defaults(feature=False)
 args = parser.parse_args()
 
 input_filepath = Path(args.input)
@@ -27,14 +33,15 @@ if input_filepath.exists() and input_filepath.is_file():
         plt.axhline(y=args.axhline, color=args.axhline_colour, linestyle=args.axhline_style)
     
     x, y = np.loadtxt(input_filepath, delimiter=',', unpack=True)
-    plt.plot(x, y, label=args.label, color=args.colour)
-    plt.xlabel('x')
-    plt.ylabel('y')
-
+    plt.plot(x, y, label=args.label, color=args.colour)   
+    plt.xlabel(args.xlabel)
+    plt.ylabel(args.ylabel)
     plt.xscale(args.xscale)
     plt.yscale(args.yscale)
 
-    plt.legend(loc='upper right')
+    if args.legend:
+        plt.legend(loc=args.legend_loc)
+    
     plt.show()
 else:
     print('Error: The provided CSV graph data file does not exist or is not a file.')
