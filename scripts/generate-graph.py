cfed2ae5fdcb4582579d941c3590fe4baa74b23b
GalacticGlum <nerftactic09@gmail.com>
2019-08-25 19:26:37 -0400

    Added style options to the generate graph script

--- scripts/generate-graph.py
+++ scripts/generate-graph.py
@@ -12,22 +12,25 @@ from pathlib import Path
 
 parser = argparse.ArgumentParser(description='Constructs a graph via matplotlib from a CSV file.')
 parser.add_argument('input', type=str, help='The filepath to the CSV file containing the graph data.')
+parser.add_argument('--label', type=str, help='The label of the graph.')
+parser.add_argument('--colour', type=str, help='The colour of the graph.', default='blue')
 parser.add_argument('--xscale', type=str, help='The x-axis scale to apply.', default='linear')
 parser.add_argument('--yscale', type=str, help='The y-axis scale to apply.', default='linear')
 parser.add_argument('--axhline', type=float, help='The horizontal axis line.')
 parser.add_argument('--axhline-style', type=str, help='The style of the horizontal axis line.', default='-')
+parser.add_argument('--axhline-colour', type=str, help='The colour of the horizontal axis line.', default='red')
 args = parser.parse_args()
 
 input_filepath = Path(args.input)
 if input_filepath.exists() and input_filepath.is_file():
     if args.axhline is not None:
-        plt.axhline(y=args.axhline, color='r', linestyle=args.axhline_style)
+        plt.axhline(y=args.axhline, color=args.axhline_colour, linestyle=args.axhline_style)
     
     x, y = np.loadtxt(input_filepath, delimiter=',', unpack=True)
-    plt.plot(x, y, label='$\\frac{\pi(x)}{x/\ln(x)}$', color='blue')
+    plt.plot(x, y, label=args.label, color=args.colour)
     plt.xlabel('x')
     plt.ylabel('y')
-    
+
     plt.xscale(args.xscale)
     plt.yscale(args.yscale)
 
