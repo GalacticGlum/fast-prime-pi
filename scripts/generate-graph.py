1a5cad973e942b2c8b7e6d1849c9e8707d326553
GalacticGlum <nerftactic09@gmail.com>
2019-08-25 19:51:10 -0400

    Added latex exporting

--- scripts/generate-graph.py
+++ scripts/generate-graph.py
@@ -24,9 +24,10 @@ parser.add_argument('--axhline-style', type=str, help='The style of the horizont
 parser.add_argument('--axhline-colour', type=str, help='The colour of the horizontal axis line.', default='red')
 parser.add_argument('--legend', dest='legend', help='Enable the legend.', action='store_true')
 parser.add_argument('--legend-loc', type=str, help='The location of the legend.', default='upper right')
-parser.add_argument('--export-tikz', dest='export_tikz', help='Export the graph as a LaTeX file using PGF/TikZ.', action='store_true')
-parser.set_defaults(legend=False)
-parser.set_defaults(export_tikz=False)
+parser.add_argument('--export-latex', dest='export_latex', help='Export the graph as a LaTeX file using PGF/TikZ.', action='store_true')
+parser.add_argument('--latex-output', type=str, help='The name of the exported LaTeX file.')
+parser.add_argument('--no-preview', dest='preview', help='Disable the graph preview window.', action='store_false')
+parser.set_defaults(legend=False, export_latex=False, preview=True)
 args = parser.parse_args()
 
 input_filepath = Path(args.input)
@@ -47,7 +48,21 @@ if input_filepath.exists() and input_filepath.is_file():
     if args.legend:
         plt.legend(loc=args.legend_loc)
     
-    plt.show()
+    if args.preview:
+        if not args.export_latex:
+            plt.show()
+        else:
+            print('Warning: Graph preview was enabled but it could not be displayed since LaTeX export was also enabled.' +
+            ' Previewing and LaTeX exporting cannot both be enabled.')
+
+    if args.export_latex:
+        import tikzplotlib
+        if args.latex_output is None:
+            parser.error('--export-latex requires --latex-output.')
+
+        output_path = Path(args.latex_output)
+        output_path.parent.mkdir(parents=True, exist_ok=True)
+        tikzplotlib.save(args.latex_output)
 else:
-    print('Error: The provided CSV graph data file does not exist or is not a file.')
+    print('Error: Could not read graph data - The provided CSV graph data file does not exist or is not a file.')
     
